b5545eeee3538a419ccfaeeb9cb11352886f9403
Chris Tacey-Green <chris@taceygreen.com>
2019-01-09 12:17:17 +0000

    Add project files.

---
+++ CheckPermutation/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+// you can also use other imports, for example:
+// using System.Collections.Generic;
+
+// you can write to stdout for debugging purposes, e.g.
+// Console.WriteLine("this is a debug message");
+
+class Solution
+{
+    public int solution(int[] A)
+    {
+        // write your code in C# 6.0 with .NET 4.5 (Mono)
+        int[] counter = new int[A.Length];
+        int limit = A.Length;
+        foreach(var item in A)
+        {
+            if(item < 1 || item > limit)
+            {
+                return 0;
+            }
+            else
+            {
+                if(counter[item - 1] != 0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    counter[item - 1] = 1;
+                }
+            }
+        }
+        return 1;
+    }
+}
